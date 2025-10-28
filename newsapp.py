bdf8a0915f22c1bf3558b0ffe534abbb9af42ebb
sharanprakashpatil <sharanprakashpatil70558@gmail.com>
2025-10-29 01:22:09 +0530

    first commit in the news app

---
+++ newsapp.py
@@ -0,0 +1,25 @@
+import requests
+import smtplib, ssl
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "useremail"
+    password = "ennter the app password"
+    reciver = "useremail"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host,port,context=context)as server:
+        server.login(username,password)
+        server.sendmail(username,reciver,message)
+
+apikey="6ec16b98333e4f94b6e3f11b01f5d916"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6ec16b98333e4f94b6e3f11b01f5d916&language=en"
+request = requests.get(url)
+content = request.json()
+for article in content["articles"][0:20]:
+    if article["title"] is not None:
+        body = "subject: Today's news"+"\n"+body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"]+  2*"\n"
+body =body.encode("utf-8")
+send_email(message = body)
+
